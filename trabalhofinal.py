19b57f083095dd30ad0f0aa339eff26a3514f9df
thiagoexe <thiagobriitto@outlook.com>
2023-03-20 02:29:58 -0300

    Applied NMS (Non-Max Supression)

--- trabalhofinal.py
+++ trabalhofinal.py
@@ -2,29 +2,39 @@ import cv2
 import numpy as np
 import time
 
-def loading_bar(current,total):
-    toolbar_width = 40
-    progress = int(current/total*toolbar_width)
-    bar = "[" + " "*(toolbar_width-progress) + "#"*(progress) + "]"
-    print(f"\r{bar} {current}/{total}", end="", flush=True)
-    time.sleep(0.1)
-
-
 # Carrega as imagens
-img1 = cv2.imread(r'D:/Python/Nova pasta/WhatsApp Image 2023-03-16 at 21.44.00 (1).jpeg', cv2.IMREAD_GRAYSCALE)
-img2 = cv2.imread(r'D:/Python/Nova pasta/WhatsApp Image 2023-03-16 at 21.44.00.jpeg', cv2.IMREAD_GRAYSCALE)
+img1 = cv2.imread(r'D:/Python/Nova pasta/WhatsApp Image 2023-03-16 at 21.44.00 (1).jpeg')
+img2 = cv2.imread(r'D:/Python/Nova pasta/WhatsApp Image 2023-03-16 at 21.44.00.jpeg')
+
+
+# Imagens em escala de cinza
+gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
 
 # Definir os parâmetros para o detector de Harris
 block_size = 2
 ksize = 3
-k = 0.001
+k = 0.01
+
+
+
+gray1 = np.float32(gray1)
+gray2 = np.float32(gray2)
 
 # Detectar os cantos nas duas imagens usando o detector de Harris
-corners1 = cv2.cornerHarris(img1, block_size, ksize, k)
-corners2 = cv2.cornerHarris(img2, block_size, ksize, k)
+corners1 = cv2.cornerHarris(gray1, block_size, ksize, k)
+corners2 = cv2.cornerHarris(gray2, block_size, ksize, k)
+
+# Aplica NMS
+nms_size = 5
+corners1_max = cv2.dilate(corners1, np.ones((nms_size, nms_size)))
+corners1[corners1 < corners1_max] = 0
+
+corners2_max = cv2.dilate(corners2, np.ones((nms_size, nms_size)))
+corners2[corners2 < corners2_max] = 0
 
 # Criar um kernel para dilatação
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (4,4))
 
 # Dilatar as regiões brancas (cantos) na imagem
 corners1_dilated = cv2.dilate(corners1, kernel)
