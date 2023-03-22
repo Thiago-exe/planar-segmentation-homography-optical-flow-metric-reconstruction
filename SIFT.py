4a2c0b5eb3098878a2b5dcad81a5673e798b56a7
Thiago Brito <thiagobriitto@ufpi.edu.br>
2023-03-22 14:46:38 -0300

    Removed match algorithm, refactored SIFT to gaussian blur in 4 octaves and 5 levels without resizing.

--- SIFT.py
+++ SIFT.py
@@ -2,12 +2,47 @@ import cv2
 import numpy as np
 
 # Load images
-img1 = cv2.imread(r'D:/Python/Nova pasta/WhatsApp Image 2023-03-16 at 21.44.00 (1).jpeg')
-img2 = cv2.imread(r'D:/Python/Nova pasta/WhatsApp Image 2023-03-16 at 21.44.00.jpeg')
+img1 = cv2.imread(r'WhatsApp Image 2023-03-16 at 21.44.00.jpeg')
+img2 = cv2.imread(r'WhatsApp Image 2023-03-16 at 21.44.00 (1).jpeg')
 
 # Convert images to grayscale
 gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+gaussian_pyramid = []
+gaussian_pyramid2 = []
+
+# define number of octaves and levels
+num_octaves = 4
+num_levels = 5
+
+# apply Gaussian blur for each octave and level
+for i in range(num_octaves):
+    octave = []
+    for j in range(num_levels):
+        # apply Gaussian blur with increasing kernel size for each level
+        ksize = (2*j+1, 2*j+1)
+        blurred = cv2.GaussianBlur(gray1, ksize, 0)
+        octave.append(blurred)
+    gaussian_pyramid.append(octave)
+    # downsample the image by half for the next octave
+    gray1 = blurred
+
+
+for i in range(num_octaves):
+    octave = []
+    for j in range(num_levels):
+        # apply Gaussian blur with increasing kernel size for each level
+        ksize = (2*j+1, 2*j+1)
+        blurred = cv2.GaussianBlur(gray2, ksize, 0)
+        octave.append(blurred)
+    gaussian_pyramid2.append(octave)
+    # downsample the image by half for the next octave
+    gray2 = blurred
+
+# display the Gaussian pyramid
+cv2.imshow('gaussian 2', gray2)
+cv2.imshow('gaussian 1', gray1)
 
 # Initialize SIFT detector
 sift = cv2.SIFT_create()
@@ -16,21 +51,11 @@ sift = cv2.SIFT_create()
 kp1, des1 = sift.detectAndCompute(gray1, None)
 kp2, des2 = sift.detectAndCompute(gray2, None)
 
-# Initialize BFMatcher
-bf = cv2.FlannBasedMatcher()
-
-# Match descriptors
-matches = bf.match(des1, des2)
-
-# Sort matches in order of distance
-matches = sorted(matches, key=lambda x: x.distance)
-
-# Draw top 10 matches
-img_matches = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+img1 = cv2.drawKeypoints(gray1,kp1,img1)
+cv2.imshow('keypoints', img1)
 
 
 # Display image
-img_matches_resized = cv2.resize(img_matches, (800, 600))
-cv2.imshow('Matches', img_matches_resized)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
